4a18044854964a281da3a311d97070ac0d7f91d2
chaniii999 <psc0729@naver.com>
2025-06-05 18:40:02 +0900

    init-Ctrl-user: 불필요 메서드 삭제

--- src/main/java/studyMate/controller/UserController.java
+++ src/main/java/studyMate/controller/UserController.java
@@ -4,14 +4,10 @@ import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import studyMate.dto.SignUpReqDto;
+import studyMate.service.UserService;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
 
-    @PostMapping("/signup")
-    public ResponseEntity<?> signup(@Valid @RequestBody SignUpReqDto requestDto) {
-        // 유효성 검사를 통과하면 서비스로 전달
-        return ResponseEntity.ok("회원가입 성공");
-    }
 }
