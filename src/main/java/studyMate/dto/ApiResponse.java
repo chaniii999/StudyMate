532fb6bb7da4efd29997ac5a969f428ac90d0dab
chaniii999 <psc0729@naver.com>
2025-06-08 17:27:08 +0900

    fix-dto-ApiResponse: 구조 변경- data 추가

--- src/main/java/studyMate/dto/ApiResponse.java
+++ src/main/java/studyMate/dto/ApiResponse.java
@@ -1,11 +1,20 @@
 package studyMate.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
-
+@Getter
+@Setter
 @AllArgsConstructor
-public class ApiResponse {
+public class ApiResponse<T> {
     private boolean success;
     private String message;
+    private T data;
+
+    public ApiResponse(boolean success, String message) {
+        this.success = success;
+        this.message = message;
+        this.data = null;
+    }
 }
