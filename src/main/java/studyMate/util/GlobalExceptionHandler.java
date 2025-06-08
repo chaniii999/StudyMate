76235013f327de0b5deec30ab7082296132a1de8
chaniii999 <psc0729@naver.com>
2025-06-08 17:27:42 +0900

    add-handler: 글로벌 익셉션 핸들러 추가

---
+++ src/main/java/studyMate/util/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package studyMate.util;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import studyMate.dto.ApiResponse;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ApiResponse> handleIllegalArgument(IllegalArgumentException ex) {
+        return ResponseEntity.badRequest().body(new ApiResponse(false, ex.getMessage()));
+    }
+}
