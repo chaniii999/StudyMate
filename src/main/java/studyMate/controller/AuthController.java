88bfdba4c6bdcea9a5048adfc02f36ab64ea036e
chaniii999 <psc0729@naver.com>
2025-06-09 12:46:29 +0900

    add-ctrl: 로그인 api 추가

--- src/main/java/studyMate/controller/AuthController.java
+++ src/main/java/studyMate/controller/AuthController.java
@@ -4,6 +4,8 @@ import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import studyMate.dto.ApiResponse;
+import studyMate.dto.SignInReq;
+import studyMate.dto.TokenDto;
 import studyMate.dto.auth.SendCodeDto;
 import studyMate.dto.auth.SignUpReqDto;
 import studyMate.dto.auth.VerifyCodeDto;
@@ -17,6 +19,12 @@ public class AuthController {
 
     private UserService userService;
     private AuthService authService;
+
+    @PostMapping("/sign-in")
+    public ApiResponse<?> signIn(@Valid @RequestBody SignInReq signInReq) {
+        TokenDto tokenDto = userService.login(signInReq);
+        return new ApiResponse<>(true, "User registered successfully!", tokenDto);
+    }
 
     @PostMapping("/sign-up")
     public ApiResponse<?> signUp(@Valid @RequestBody SignUpReqDto signUpReqDto) {
