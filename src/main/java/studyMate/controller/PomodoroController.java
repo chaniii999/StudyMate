65ea18e96727622d39eb1375f6994cedb5ee8094
chaniii999 <psc0729@naver.com>
2025-06-10 13:06:53 +0900

    add: 타이머 기능 준비

--- src/main/java/studyMate/controller/PomodoroController.java
+++ src/main/java/studyMate/controller/PomodoroController.java
@@ -1,18 +1,27 @@
 package studyMate.controller;
 
+import lombok.AllArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import studyMate.dto.ApiResponse;
 import studyMate.dto.pomodoro.PomodoroStartReqDto;
 import studyMate.entity.User;
+import studyMate.service.TimerService;
 
+@AllArgsConstructor
 @RestController
 @RequestMapping("/api/timer")
 public class PomodoroController {
+    // 타이머기능은 Websocket을 쓰기로했음.
+    // 백에서는 요청만, 실질적기능은 프론트에서.
+
+    private final TimerService timerService;
+
 
     @PostMapping("/start")
     public ApiResponse<?> startTimer(@AuthenticationPrincipal User user,
                                    @RequestBody PomodoroStartReqDto dto) {
+        timerService.startTimer(user, dto);
         return new ApiResponse<>(true, "Timer started for user: " + user.getNickname());
     }
 
---
+++ src/main/java/studyMate/service/TimerService.java
@@ -0,0 +1,14 @@
+package studyMate.service;
+
+import org.springframework.stereotype.Service;
+import studyMate.dto.pomodoro.PomodoroStartReqDto;
+import studyMate.entity.User;
+
+
+@Service
+public class TimerService {
+
+    public void startTimer(User user, PomodoroStartReqDto dto) {
+
+    }
+}
