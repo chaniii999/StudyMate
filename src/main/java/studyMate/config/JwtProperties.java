1168215dfbd33cd7bf0763adc92430a04f52c4ef
chan <psc0729@naver.com>
2025-06-19 19:48:26 +0900

    refactor: JwtProperties 클래스의 필드 변경 및 Setter 추가

--- src/main/java/studyMate/config/JwtProperties.java
+++ src/main/java/studyMate/config/JwtProperties.java
@@ -1,20 +1,14 @@
 package studyMate.config;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @Getter
+@Setter
 @ConfigurationProperties(prefix = "spring.jwt")
 public class JwtProperties {
-    private final String secretKey;
-    private final long accessTokenValidityInSeconds;
-    private final long refreshTokenValidityInSeconds;
-
-    public JwtProperties(String secretKey, 
-                        long accessTokenValidityInSeconds, 
-                        long refreshTokenValidityInSeconds) {
-        this.secretKey = secretKey;
-        this.accessTokenValidityInSeconds = accessTokenValidityInSeconds;
-        this.refreshTokenValidityInSeconds = refreshTokenValidityInSeconds;
-    }
+    private String secretKey;
+    private long accessTokenValidityInSeconds;
+    private long refreshTokenValidityInSeconds;
 } 
